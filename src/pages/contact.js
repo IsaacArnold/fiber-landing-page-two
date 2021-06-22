9ccc886315c6d2cd1241cb008d1679bbeb6ea7b3
Isaac Arnold <isaacarnold@icloud.com>
2021-06-22 21:01:40 +1200

    Updating styles of Layout component and contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,24 +2,50 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
 
+import "../styles/normalize.css";
+import "../styles/contact.scss";
+
 const ContactFormPage = () => {
   return (
     <Layout>
       <Helmet></Helmet>
-      <h1>Contact</h1>
+      <div className="content-container">
+        <h1>Contact Us</h1>
 
-      <form name="Contact Form" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="Contact Form" />
-        <div>
-          <label htmlFor="email">Your Email:</label>
-          <input type="email" name="email" id="email" />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" id="message" />
-        </div>
-        <button type="submit">Send</button>
-      </form>
+        <form name="Contact Form" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="Contact Form" />
+          <div className="form-section">
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              id="fullName"
+              placeholder="John Doe"
+              required={true}
+            />
+          </div>
+          <div className="form-section">
+            <label htmlFor="email">Your Email:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="john@example.com"
+              required={true}
+            />
+          </div>
+          <div className="form-section">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Type your message here..."
+              required={true}
+            />
+          </div>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </Layout>
   );
 };
